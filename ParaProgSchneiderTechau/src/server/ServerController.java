9ca12a7837d22cef6f713132547002d8218a127b
peetz85 <pascalschneider@studenten.hs-bremerhaven.de>
2014-07-15 12:03:19 +0200

    Server Install

--- ParaProgSchneiderTechau/src/server/ServerController.java
+++ ParaProgSchneiderTechau/src/server/ServerController.java
@@ -33,7 +33,7 @@ public class ServerController extends Thread implements CSProcess{
         // NetChannelServer initializierung
         try {
             //Initialize a Node that does not have a CNS client
-            key = Node.getInstance().init(new XMLNodeFactory("src/server/nocns.xml"));
+            key = Node.getInstance().init(new XMLNodeFactory("ParaProgSchneiderTechau/src/server/nocns.xml"));
             localNodeID = Node.getInstance().getNodeID();
             //Initialize the CNS Server Process
             CNS.install(key);
